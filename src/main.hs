942364486089cb968a0acc1b677c938b9aaaab7b
lesguillemets <lesguillemets@users.noreply.github.com>
2015-03-13 16:52:09 +0900

    separate onboxclick

--- src/main.hs
+++ src/main.hs
@@ -2,6 +2,9 @@ import Haste
 import Haste.DOM
 import Haste.Graphics.Canvas
 import Data.IORef
+import Data.Array
+import Data.Array.MArray
+import Data.Array.IO
 
 colors = [
          "#000000", "#484848", "#b8b8b8", "f8f8f8",
@@ -14,27 +17,33 @@ square' :: Shape ()
 square' = do
     rect (10,10) (20,20)
 
-setUp :: IORef Brush -> IO ()
-setUp brush = do
+setUp :: Env -> IO ()
+setUp env = do
+    let brush = _brush env
     colorboxes <- elemsByClass "colorbox"
-    mapM_ (\(e,c) -> setStyle e "background-color" c) $
-        zip colorboxes colors
-    mapM_ (\e -> (onEvent e OnClick (
-          \_ _-> do
-            getStyle e "background-color" >>= modifyIORef brush . flip setBrush
-            readIORef brush >>= print
-          ))
-          ) colorboxes
+    mapM_ (\(e,c) -> setStyle e "background-color" c) $ zip colorboxes colors
+    mapM_ (\(e,i) -> (onEvent e OnClick (onBoxClick brush (e,i)))
+          ) (zip colorboxes [0..])
 
-setBrush :: Brush -> String -> Brush
-setBrush b c = b { _color = c}
+onBoxClick :: IORef Brush -> (Elem, Int) -> Int -> (Int,Int) -> IO ()
+onBoxClick brush (elm,n) = \ _ _ -> do
+    newcolor <- getStyle elm "background-color"
+    modifyIORef brush (setBrush newcolor n)
+    readIORef brush >>= putStrLn . (++ " on " ++ (show n)) . show
+
+setBrush :: String -> Int -> Brush -> Brush
+setBrush c n b = b { _color = c, _cid = n}
 
 main = do
     Just canv <- getCanvasById "canv"
-    _brush <- newIORef $ Brush (head colors)
-    setUp _brush
+    b <- newIORef $ Brush (head colors) 0
+    arr <- newArray (0,10) 15 :: IO (IOArray Int Int)
+    let env = Env b arr
+    setUp env
     alert "Hi there!"
     render canv $ do
         color (RGB 234 234 52) . fill $ square'
 
-data Brush = Brush { _color :: String } deriving (Show)
+data Brush = Brush { _color :: String , _cid :: Int} deriving (Show)
+type Pixels = IOArray Int Int
+data Env = Env {_brush :: IORef Brush , _pixs :: Pixels}
