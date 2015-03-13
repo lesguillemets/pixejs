09411622481bec05c17340278f0d91a78b875f56
lesguillemets <lesguillemets@users.noreply.github.com>
2015-03-13 15:16:24 +0900

    set brush colors

--- src/main.hs
+++ src/main.hs
@@ -1,6 +1,7 @@
 import Haste
 import Haste.DOM
 import Haste.Graphics.Canvas
+import Data.IORef
 
 colors = [
          "#000000", "#484848", "#b8b8b8", "f8f8f8",
@@ -13,16 +14,27 @@ square' :: Shape ()
 square' = do
     rect (10,10) (20,20)
 
-setUp :: IO ()
-setUp = do
+setUp :: IORef Brush -> IO ()
+setUp brush = do
     colorboxes <- elemsByClass "colorbox"
     mapM_ (\(e,c) -> setStyle e "background-color" c) $
         zip colorboxes colors
-    mapM_ (\e -> (onEvent e OnClick (\_ _-> print "HI"))) colorboxes
+    mapM_ (\e -> (onEvent e OnClick (
+          \_ _-> do
+            getStyle e "background-color" >>= modifyIORef brush . flip setBrush
+            readIORef brush >>= print
+          ))
+          ) colorboxes
+
+setBrush :: Brush -> String -> Brush
+setBrush b c = b { _color = c}
 
 main = do
     Just canv <- getCanvasById "canv"
-    setUp
+    _brush <- newIORef $ Brush (head colors)
+    setUp _brush
     alert "Hi there!"
     render canv $ do
         color (RGB 234 234 52) . fill $ square'
+
+data Brush = Brush { _color :: String } deriving (Show)
